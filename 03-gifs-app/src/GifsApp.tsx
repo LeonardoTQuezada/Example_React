c80068c13db3293fe7da3fc78e7e52223b61a504
LeonardoTQuezada <leonardo18.tinitana@gmail.com>
2026-09-17 16:40:51 +0200

    seccion 6 parte 2

--- 03-gifs-app/src/GifsApp.tsx
+++ 03-gifs-app/src/GifsApp.tsx
@@ -1,8 +1,22 @@
 
+import { GifList } from './gifs/components/GifList';
+import { PreviousSearches } from './gifs/components/PreviousSearches';
+import { mockGifs } from './mock-data/gifs.mock';
+import { CustomHeader } from './shared/components/CustomHeader';
+import { SearchBar } from './shared/components/SearchBar';
+
+
 export const GifsApp = () => {
   return (
     <>
-    
+      {/* Header */}
+      <CustomHeader title="Buscador de Gifs" description="Descubre y comparte el Gif perfecto" />
+      {/* Search */}
+      <SearchBar placeholder='Busca lo que quieras'/>
+      {/* Búsquedas previas */}
+      <PreviousSearches searches={['Goku', 'Dragon Ball', 'Vegueta']} />
+      {/* Gifs */}
+      <GifList gifs={ mockGifs} />
     </>
-  )
-}
+  );
+};
