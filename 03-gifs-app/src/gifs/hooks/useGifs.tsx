0fc78a39a550fd9424c7ee207b62c89883bbf621
LeonardoTQuezada <leonardo18.tinitana@gmail.com>
2026-09-18 14:02:51 +0200

    Seccion 7 useRef para hacer una cache

--- 03-gifs-app/src/gifs/hooks/useGifs.tsx
+++ 03-gifs-app/src/gifs/hooks/useGifs.tsx
@@ -1,17 +1,24 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { getGifsByQuery } from '../actions/get-gifs-by-query.action';
 import type { Gif } from '../interfaces/gif.interface';
+
+// const gifsCache: Record<string, Gif[]> = {};
 
 export const useGifs = () => {
   // para traer los gifs
   const [gifs, setGifs] = useState<Gif[]>([]);
 
   const [previousTerms, setPreviousTerms] = useState<string[]>([]);
+//hook para mantener el valor cuando se renderiza useRef
+  const gifsCache = useRef<Record<string, Gif[]>>({});
 
   const handleTermClicked = async (term: string) => {
-      const gifs = await getGifsByQuery(term);
-      setGifs(gifs);
-      console.log("entro")
+    if (gifsCache.current[term]) {
+      setGifs(gifsCache.current[term]);
+      return;
+    }
+    const gifs = await getGifsByQuery(term);
+    setGifs(gifs);
   };
 
   const handleSearch = async (query: string = '') => {
@@ -27,15 +34,18 @@ export const useGifs = () => {
     const gifs = await getGifsByQuery(query);
     //establecer los gifs
     setGifs(gifs);
+
+    gifsCache.current[query] = gifs;
+    console.log(gifsCache);
   };
 
   return {
-     // Properties
+    // Properties
     gifs,
 
     // Methods
     handleSearch,
     handleTermClicked,
-    previousTerms,
+    previousTerms
   };
 };
